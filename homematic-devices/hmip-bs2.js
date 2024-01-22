19c3f267d3be17ecc56d0711df59f46a3d093c8a
cheinelt <carsten@heinelt.eu>
2024-01-22 09:25:14 +0100

    added HmIP-BS2

---
+++ homematic-devices/hmip-bs2.js
@@ -0,0 +1,12 @@
+const Accessory = require('./lib/accessory');
+
+module.exports = class HmipBsm2 extends Accessory {
+    init(config) {
+        this.addService('Lightbulb', config.name)
+            .get('On', config.deviceAddress + ':4.STATE')
+            .set('On', config.deviceAddress + ':4.STATE');
+        this.addService('Lightbulb', config.name)
+            .get('On', config.deviceAddress + ':8.STATE')
+            .set('On', config.deviceAddress + ':8.STATE'); 
+    }
+};
